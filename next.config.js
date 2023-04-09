6809bae80dc669fc86185e4e17f2a506a369f373
dscrogg399 <dscrogg1@uab.edu>
2023-04-09 18:54:13 -0500

    fixed tooltips

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  swcMinify: false,
   images: {
     domains: ["cdn-icons-png.flaticon.com", "i.imgur.com"],
   },
